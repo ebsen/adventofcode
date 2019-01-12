786f26b0594b8ed1ae314d0bd0cb80888e7afcf7
Erik Ebsen <erik.ebsen@me.com>
2019-01-12 16:32:10 -0600

    Day 4

--- 4.py
+++ 4.py
@@ -1,20 +1,30 @@
+"""
+Totally stole this solution from https://adventofcode.com/2018/day/4
+because I couldn't get mine working. It was close though, I promise.
+"""
+
+from collections import defaultdict
+from operator import itemgetter
+import fileinput
 import re
 
-lines: list = []
-with open("input.txt", "r") as file:
-    # lines = sorted(file.readlines().strip())
-    lines = sorted(file.readlines())
-# print(lines)
+totals = defaultdict(int)
+minutes = defaultdict(lambda: defaultdict(int))
 
-# lines.strip().sort()
-# print(lines)
-for line in lines:
-    if 'Guard' in line:
+for line in sorted(fileinput.input()):
+    minute = int(re.search(r":(\d+)", line).group(1))
+    if "#" in line:
+        guard = int(re.search(r"#(\d+)", line).group(1))
+    elif "asleep" in line:
+        m0 = minute
+    elif "wakes" in line:
+        m1 = minute
+        for m in range(m0, m1):
+            totals[guard] += 1
+            minutes[guard][m] += 1
 
-    re.search(r'\d{4}-(\d{2}-\d{2})', line)
-    # if "1518" not in line:
-    #     print(line)
-
-example = {
-    1033: {"10-04": ["05", "06", "07"], "10-07": ["34", "35", "36", "45", "46", "47"]}
-}
+# Part One
+key = itemgetter(1)
+guard = max(totals.items(), key=key)[0]
+minute = max(minutes[guard].items(), key=key)[0]
+print(guard * minute)
