d04200aa08b09743e9601e919a958168f55077de
Erik Ebsen <erik.ebsen@me.com>
2023-02-05 19:13:51 +0000

    Clean up a little more

--- schema.rb
+++ schema.rb
@@ -4,12 +4,11 @@ require 'sqlite3'
 def init_db
   DB.create_table :advent do
     primary_key   :id
-    Integer       :day,     null: false, unique: true
+    Integer       :day,       null: false, unique: true
     String        :input
     Integer       :solution1
     Integer       :solution2
   end
 
-  # records = DB.from :advent
   DB.from :advent
 end
